7b20392dde4f4ec5f61d6dab30d6447fa7fe718a
Julian Jacklin <42346111+Julianj22@users.noreply.github.com>
2021-07-13 11:23:31 +0000

    final

---
+++ script.js
@@ -0,0 +1,24 @@
+$("p").fadeIn(2000);
+// $("#homePageImg").fadeIn(1500);
+$("#contacts").fadeIn(1500);  
+$(".grid").delay(1500).fadeIn(2000); 
+$(".bot").delay(1500).fadeIn(2000);
+// $("hr").fadeIn(4000).delay(1500); 
+
+$(".q1").delay(1000).fadeIn(1000);
+$(".q2").delay(2000).fadeIn(1000); 
+$("#q3").delay(3000).fadeIn(1000); 
+$("#book").delay(4000).fadeIn(1000); 
+$("#buying").delay(4000).fadeIn(1000); 
+$(".bott").delay(5000).fadeIn(2000);
+// $("#anti").delay(5000).fadeIn(1000);  
+
+$("#p1").delay(500).fadeIn(500);
+$("#p2").delay(1000).fadeIn(500);
+$("#p3").delay(1500).fadeIn(500);
+$(".row1").delay(1500).fadeIn(500);
+$("#p4").delay(2000).fadeIn(500);
+$("#p5").delay(2500).fadeIn(500);
+$("#p6").delay(3000).fadeIn(500);
+$(".row2").delay(3000).fadeIn(500);
+$(".bottt").delay(3000).fadeIn(1000);
